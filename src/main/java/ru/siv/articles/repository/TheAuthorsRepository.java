40e871538e0dfa76589a3f7cc9ac2ceaef285e37
Serg I <iserg009@yandex.ru>
2021-12-06 11:20:23 +0300

    Добавлен репозиторий авторов.

---
+++ src/main/java/ru/siv/articles/repository/TheAuthorsRepository.java
@@ -0,0 +1,15 @@
+package ru.siv.articles.repository;
+
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.stereotype.Repository;
+import ru.siv.articles.model.TheAuthors;
+
+import java.util.List;
+
+@Repository
+public interface TheAuthorsRepository extends CrudRepository<TheAuthors, Integer> {
+  TheAuthors findByNameIgnoreCase(String name);
+  @Query("SELECT ta FROM TheAuthors ta ORDER BY ta.name")
+  List<TheAuthors> filterAllOrderByName();
+}
