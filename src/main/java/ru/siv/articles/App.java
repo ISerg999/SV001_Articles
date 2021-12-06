c3e9275c8ff7155c9bb31680054560ab069670c6
Serg I <iserg009@yandex.ru>
2021-12-06 09:50:35 +0300

    Инициализация.

---
+++ src/main/java/ru/siv/articles/App.java
@@ -0,0 +1,11 @@
+package ru.siv.articles;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class App {
+  public static void main(String[] args) {
+    SpringApplication.run(App.class, args);
+  }
+}
